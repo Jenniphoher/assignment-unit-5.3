289eb8706f90dd064763e3860f43efbb7fc6badb
Jennifer Her <jenniher0492@gmail.com>
2024-07-08 01:53:08 -0500

    extra stretch done

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -90,50 +90,127 @@ function trackList(collection, title, name, duration) {
     name,
     duration
   } 
-  if(trackArray.indexOf(title) != -1) {
-    console.log(`New track added to: ${title}`);
-  } else {
+  if(trackArray.indexOf(title) === -1) {
     trackArray.push(title);
     trackArray[title] = [];
-  }
-
-  for(album of collection) {
-    if(album.title === title) {
-      trackArray[title].push(track);
-    }
-  } return trackArray;
+    console.log(`Created: ${title}`);
+    for(let album of collection) {
+      if(album.title === title) {
+        trackArray[title].push(track);
+        return trackArray;
+      }
+    } 
+  } else {
+    console.log(`New track added to: ${title}`);
+    for(let album of collection) {
+      if(album.title === title) {
+        trackArray[title].push(track);
+        return trackArray;
+      }
+    } 
+  } 
 }
 
 console.log(trackList(myCollection, 'Eye of the Storm', 'Change', 2 + ':' + 56));
 console.log(trackList(myCollection, 'Eye of the Storm', 'Stand Out Fit In', 3 + ':' + 34));
-console.log(trackList(myCollection, 'Eye of the Storm', 'Wasted Nights', 3 + ':' + 44));
-// console.log(addTracks('Stand Out Fit In', 3 + ':' + 34));
-// console.log(addTracks('Wasted Nights', 3 + ':' + 44));
+console.log(trackList(myCollection, 'First Love', 'Automatic', 5 + ':' + 28));
+console.log(trackList(myCollection, 'First Love', 'First Love', 4 + ':' + 18));
+console.log(trackList(myCollection, 'Atlas: I', 'Saturn', 4 + ':' + 50));
+console.log(trackList(myCollection, 'Atlas: I', 'Light', 4 + ':' + 23));
+console.log(trackList(myCollection, 'Purpose', 'Sorry', 3 + ':' + 20));
+console.log(trackList(myCollection, 'Purpose', 'Where Are U Now', 4 + ':' + 0o5));
+console.log(trackList(myCollection, 'Justice', 'Hold On', 2 + ':' + 51));
+console.log(trackList(myCollection, 'Justice', 'Ghost', 2 + ':' + 33));
+console.log(trackList(myCollection, 'Where the Earth Meets the Sky', 'The Highlands', 2 + ':' + 50));
+console.log(trackList(myCollection, 'Where the Earth Meets the Sky', 'Baby Seal', 1 + ':' + 37));
 
+console.log('====== ADD UPDATE FUNCTION ======');
 
 function addToCollection(collection, title, artist, yearPublished, tracks) {
   let album = {
     title,
     artist,
     yearPublished,
-    
   }
   
-  if(tracks != 'array') {
-    console.log('Tracks added later.');
+  if(typeof tracks != 'undefined') {
+    for(let name of collection) {
+      if(name.title === tracks[tracks.indexOf(title)]) {
+        name.songs = tracks[title];
+        return collection;
+      } } 
+  } else {
     collection.push(album);
+    console.log('Tracks added later.');
     return album;
-  } else if(tracks === 'array') {
-    for(let album of collection) {
-      if(collection[album.title] === tracks[album]) {
-        Object.assign(collection[album], tracks[album]);
-        console.log('Track added.');
-      } 
-    }
-  } return collection;
+  }
 } 
 
-console.log(myCollection, 'Eye of the Storm', null, null, trackArray);
+console.log(addToCollection(myCollection, 'Eye of the Storm', '', '', trackArray));
+console.log(addToCollection(myCollection, 'First Love', '', '', trackArray));
+console.log(addToCollection(myCollection, 'Atlas: I', '', '', trackArray));
+console.log(addToCollection(myCollection, 'Purpose', '', '', trackArray));
+console.log(addToCollection(myCollection, 'Justice', '', '', trackArray));
+console.log(addToCollection(myCollection, 'Where the Earth Meets the Sky', '', '', trackArray));
+
+
+
+
+
+console.log('====== UPDATE SHOW FUNCTION ======');
+
+function showCollection(collection, tracks) {
+  if(typeof tracks != 'undefined') {
+    let withSongs;
+    for(album of collection) {
+      let albumSong = album.songs;
+      withSongs = console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}:\n1. ${albumSong[0].name}: ${albumSong[0].duration}\n2. ${albumSong[1].name}: ${albumSong[1].duration}`);
+    } return withSongs;
+  } else {
+    for(album of collection) {
+      console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+    }
+  }
+}
+
+console.log(showCollection(myCollection, trackArray));
+
+
+
+
+console.log('====== SEARCH UPDATE FUNCTION ======');
+
+function search(collection, searchCriteria, trackCriteria) { 
+  let results = [];
+  if (typeof trackCriteria === 'object') {
+    for(album of collection) {
+      let albumSong = album.songs;
+      if(trackCriteria.name === albumSong[0].name) {
+        results.push(albumSong[0].name);
+        return results;
+      } else if(trackCriteria.name === albumSong[1].name) {
+        results.push(albumSong[1].name);
+        return results;
+      } 
+    } return 'No Song Title match.'
+  } else if(typeof trackCriteria != 'object' || Object.keys(trackCriteria).length === 0 || trackCriteria.name === '' ) {
+    for(album of collection) {
+      if(typeof searchCriteria != 'object' || Object.keys(searchCriteria).length === 0 || searchCriteria.artist === '' || searchCriteria.yearPublished === '') {
+        return collection;
+      } else if(searchCriteria.artist === album.artist && searchCriteria.yearPublished === album.yearPublished) {
+        results.push(album);
+      }
+    } return results;
+  } 
+}
+
+console.log(search(myCollection, {artist: 'Ray Charles', yearPublished: 1957}, {name: 'Stand Out Fit In'}));
+console.log(search(myCollection, {artist: 'ONE OK ROCK', yearPublished: 2019}, {name: 'Saturn'})); 
+console.log(search(myCollection, {}, {name: 'Dynamite'}));
+console.log(search(myCollection, {artist: 'Taya'}, {}));
+console.log(search(myCollection, {artist: 'Justin Bieber', yearPublished: 2015})); 
+
+
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
